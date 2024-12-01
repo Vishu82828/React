3b9ce1d1c44e059828bcb2ad4556b5ed471f26cd
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-12-01 21:22:32 +0530

    new project

--- Project3/src/assets/App.jsx
+++ Project3/src/assets/App.jsx
@@ -3,7 +3,23 @@ function App() {
 
   return (
     <>
-      <Routers/>
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path='SingIn' element={<SingIn/>} />
+          <Route path='SingUp' element={<SingUp/>} />
+          <Route path='Home' element={<Home/>} />
+          <Route path='About' element={<About/>} />
+          <Route path='Contact' element={<Contact/>} />
+          <Route path='Services' element={<Services/>} />
+          <Route path='Blog' element={<Blog/>} />
+          <Route path='Cart' element={<Cart/>} />
+          <Route path='Checkout' element={<Checkout/>} />
+          <Route path='Profile' element={<Profile/>} />
+          <Route path='Admin' element={<Admin/>} />
+          <Route path='*' element={<NotFound/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
