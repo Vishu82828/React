6cc981f7cae9d9fe671ab6e9dff9dce2bb41d055
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-12-02 14:58:39 +0530

    work in progress

--- Project3/src/Components/Nav1.jsx
+++ Project3/src/Components/Nav1.jsx
@@ -1,35 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Home from './Home';
+import "../Styles/Nav.css";
 
 function Nav1() {
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const navItems = [
+    { id: 1, path: "Home", name: "Home" },
+    { id: 2, path: "Bolg", name: "Blogs" },
+    { id: 3, path: "Services", name: "Services" },
+    { id: 4, path: "About", name: "About" },
+    { id: 5, path: "Contact", name: "Contact" }
+  ];
+
+  const navIcons = [
+    { id: 6, path: "SingIn", name: "Sign In" },
+    { id: 7, path: "SingUp", name: "Sign Up" },
+    { id: 8, path: "Cart", name: "Cart" },
+    { id: 9, path: "Checkout", name: "Checkout" }
+  ];
+
   return (
     <>
-    <nav className='navbar'>
-      <div className='navLogo'>
-        <Link className='navBrand' to="Home">Logo</Link>
-      </div>
-      <div className='navSection'>
-        <Link className='navHome' to="Home">Home</Link>
-        <Link className='navBlogs' to="Bolg">Blogs</Link>
-        <Link className='navServices' to="Services">Services</Link>
-        <Link className='navAbout' to="About">About</Link>
-        <Link className='navContact' to="Contact">Contact</Link>
-        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">DropDown</NavDropdown> */}
-      </div>
-      <div className='navIcon'>
-        <Link className='navSingIn' to="SingIn">Sing In</Link>
-        <Link className='navSingUp' to="SingUp">Sign Up</Link>
-        <Link className='navCart' to="Cart">Cart</Link>
-        <Link className='navCheckout' to="Checkout">Checkout</Link>
-      </div>
-    </nav>
+      <nav className='navbar'>
+        <div className='navLogo'>
+          <Link className='navBrand' to="Home">Logo</Link>
+          <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleNav}>
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+        </div>
+        <div className={`navSection ${isOpen ? 'open' : ''}`}>
+          {navItems.map(item => (
+            <Link key={item.id} className={`navtext nav${item.name}`} to={item.path}>{item.name}</Link>
+          ))}
+        </div>
+        <div className={`navIcon ${isOpen ? 'open' : ''}`}>
+          {navIcons.map(icon => (
+            <Link key={icon.id} className={`navtext nav${icon.name}`} to={icon.path}>{icon.name}</Link>
+          ))}
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Nav1;
