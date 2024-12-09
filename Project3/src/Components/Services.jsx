1b55d072f4797e477d5b8458948266a694f41a73
Vishwajeet Singh <vishwajeetsingh8422@gmail.com>
2024-12-09 18:04:47 +0530

    toastify

--- Project3/src/Components/Services.jsx
+++ Project3/src/Components/Services.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+  
+  function Services(){
+    const notify = () => toast("Wow so easy!");
 
-function Services() {
-  return (
-    <>
-      <h1>Services</h1>
-    </>
-  )
-}
+    return (
+      <div>
+        <h1>Services</h1>
+        <button onClick={notify}>Notify!</button>
+        <ToastContainer />
+      </div>
+    );
+  }
 
 export default Services
